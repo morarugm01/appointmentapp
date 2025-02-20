a21a5aa5bf9e4c287e55002addd918bbebb5ae8f
@marin <marin@falcontrading.ro>
2025-02-20 22:44:05 +0200

    layout + sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,29 @@
 import './bootstrap';
 
 import { createApp } from 'vue' 
-import OrganizationIndex from './pages/Organization/Index.vue' 
 import { createRouter, createWebHistory } from 'vue-router';
+import App from './Layouts/ceo/Layout.vue';
+import Dashboard from './pages/Ceo/Dashboard/Dashboard.vue';
+import OrganizationIndex from './pages/Ceo/Organization/Index.vue' 
+import OrganizationShow from './pages/Ceo/Organization/Show.vue';
+import PlanIndex from './pages/Ceo/Plan/Index.vue';
+import PlanShow from './pages/Ceo/Plan/Show.vue';
+import Settings from './pages/Ceo/Settings.vue';
  
 
 const routes = [
-    {path: '/', component: OrganizationIndex },
+    {path: '/ceo/dashboard', component: Dashboard },
+    {path: '/ceo/organizations/index', component: OrganizationIndex},
+    {path: '/ceo/organizations/show', component: OrganizationShow},
+    {path: '/ceo/plans/index', component: PlanIndex},
+    {path: '/ceo/plans/show', component: PlanShow},
+    {path: '/ceo/settings', component: Settings}
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
-createApp({}) 
+createApp(App) 
     .use(router)
     .mount('#app')
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,7 @@ use App\Http\Controllers\OrganizationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
-Route::get('ceo/organizations', [OrganizationController::class, 'index'])->name('ceo.organizations.index');
+Route::get('ceo/organization', [OrganizationController::class, 'index'])->name('ceo.organization.index');
 
 
